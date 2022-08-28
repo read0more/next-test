b7b7bd368d9379b8b3d0b023d83b500a8daf9b38
read0more <read0more@gmail.com>
2022-08-28 14:18:43 +0900

    Initial commit from Create Next App

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  trailingComma: 'es5', // Trailing commas where valid in ES5 (objects, arrays, etc.). No trailing commas in type parameters in TypeScript.
+  tabWidth: 2,
+  singleQuote: true,
+  endOfLine: 'lf',
+};
