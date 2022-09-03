7e6ae395fc4ae8ec26ad938732c772e4a1586a94
read0more <read0more@gmail.com>
2022-09-03 12:58:43 +0900

    ✨SWR + infinite scroll

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,45 +1,29 @@
 import axios from 'axios';
 import { NextPage } from 'next';
 import React, { useEffect, useRef, useState } from 'react';
-import Li, { Props } from '../../components/Li';
+import Li from '../../components/Li';
 import { DEFAULT_LIMIT } from '../api/hello';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import useSWRInfinite from 'swr/infinite';
 import HelloInterface from '../api/HelloInterface';
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const ProductList: NextPage = () => {
-  const [lis, setLis] = useState<Props[]>([]);
   const root = useRef(null);
   const [target, setTarget] = useState<HTMLElement | null>(null);
-  const router = useRouter();
 
-  const handleRouteChange = () => {
-    sessionStorage.setItem('s', `${window.scrollY}`);
-  };
+  const { data, size, setSize } = useSWRInfinite<{
+    data: HelloInterface[];
+  }>((pageIndex, previousPageData) => {
+    if (previousPageData?.data && !previousPageData.data.length) return null;
 
-  const { data, size, setSize } = useSWRInfinite<{ data: HelloInterface[] }>(
-    (offset) => `api/hello?offset=${offset}&limit=${DEFAULT_LIMIT}`,
-    fetcher
-  );
+    if (pageIndex === 0) return `api/hello?&limit=${DEFAULT_LIMIT}`;
 
-  console.log(data);
-
-  useEffect(() => {
-    if (!data) return;
-    console.log(data[0]);
-    setLis([...lis, ...data[0].data]);
-    // offset.current += limit.current;
-  }, [data]);
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, [router.asPath, router.events]);
+    return `api/hello?offset=${
+      pageIndex * DEFAULT_LIMIT
+    }&limit=${DEFAULT_LIMIT}`;
+  }, fetcher);
 
   useEffect(() => {
     if (!target) return;
@@ -59,32 +43,34 @@ const ProductList: NextPage = () => {
 
     const observer = new IntersectionObserver((entries, observer) => {
       if (!entries[0].isIntersecting) return;
-      setTimeout(() => setSize(size + 1), 1000);
       observer.disconnect();
-      console.log('end.');
+      setSize(size + 1);
     }, options);
     observer.observe(target);
-  }, [lis, target]);
+  }, [size, setSize, target]);
 
   return (
     <ul ref={root}>
-      {lis.map((li) => (
-        <div key={li.index}>
-          <Li index={li.index} img={li.img} />
-          <Link href={`/product/${li.index}`}>
-            <a
-              style={{
-                background: 'brown',
-                width: '500px',
-                height: '100px',
-                display: 'block',
-              }}
-            >
-              Go To {li.index}
-            </a>
-          </Link>
-        </div>
-      ))}
+      {data &&
+        data.map(({ data: dataElement }) =>
+          dataElement.map((li) => (
+            <div key={li.index}>
+              <Li index={li.index} img={li.img} />
+              <Link href={`/product/${li.index}`}>
+                <a
+                  style={{
+                    background: 'brown',
+                    width: '500px',
+                    height: '100px',
+                    display: 'block',
+                  }}
+                >
+                  Go To {li.index}
+                </a>
+              </Link>
+            </div>
+          ))
+        )}
       <div ref={setTarget}></div>
     </ul>
   );
