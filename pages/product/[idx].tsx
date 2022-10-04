97407a1e38d7a24f477fe266c62eed7ea2ef4cd2
read0more <read0more@gmail.com>
2022-10-04 23:45:41 +0900

    ✨ sass

--- pages/product/[idx].tsx
+++ pages/product/[idx].tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import styles from './product.module.scss';
 
 const Product: NextPage<{ img: string; id: string }> = () => {
   return (
@@ -12,7 +13,12 @@ const Product: NextPage<{ img: string; id: string }> = () => {
         width={1000}
         height={500}
       ></Image>
-      <Link href={`/product`}>리스트로</Link>
+      <Link href={`/product`}>
+        <div className={styles.box}>
+          <em>아무개아무개</em>
+          <b className={styles.text}>리스트로</b>
+        </div>
+      </Link>
     </div>
   );
 };
